ab2b84054ae2adbcb9e2d27a6a2113dafcb51eb7
Nicole Bradford <nbradf2@gmail.com>
2018-02-25 16:08:32 -0600

    add ledger entry schema

---
+++ budget/models.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+
+const ledgerEntrySchema = mongoose.Schema({
+	month: {type: Number, required: true},
+	day: {type: Number, required: true},
+	year: {type: Number, required: true},
+	amount: {type: Number, required: true},
+	label: {type: String},
+	type: {type: String},
+	category: {type: String, required: true}
+})
+
+const LedgerEntry = mongoose.model('LedgerEntry', ledgerEntrySchema)
+
+module.exports = {LedgerEntry};
