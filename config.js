c5474d17ca9b0d841b058ef657a676ff19386163
Nicole Bradford <nbradf2@gmail.com>
2018-02-25 14:47:36 -0600

    udpate config

--- config.js
+++ config.js
@@ -1,3 +1,9 @@
+exports.DATABASE_URL = 
+	process.env.MONGODB_URI ||
+	global.DATABASE_URL ||
+	'mongodb://localhost/budget';
+
+
 module.exports = {
 	CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || 'http://localhost:3000'
 };
