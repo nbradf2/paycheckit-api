9c31b3942468cbab3dd13503ad58b7167efee6d4
Nicole Bradford <nbradf2@gmail.com>
2018-02-28 17:56:19 -0600

    add PUT request

--- budget/router.js
+++ budget/router.js
@@ -66,6 +66,31 @@ router.post('/', jsonParser, (req, res) => {
 	res.status(201).json(item);
 });
 
+router.put('/:id', jsonParser, (req, res) => {
+	const requiredFields = ['month', 'day', 'year', 'amount', 'category'];
+	for (let i=0; i<requiredFields.length; i++) {
+		const field = requiredFields[i];
+		if (!(field in req.body)) {
+			const message = `Missing ${field} in request body`
+			console.error(message);
+			return res.status(400).send(message);
+		}
+	}
+	if (req.params.id !== req.body.id) {
+		const message = `Request path id ${req.params.id} and request body id ${req.body.id} must match`;
+		console.error(message);
+		return res.status(400).send(message);
+	}
+	console.log(`Updating ledger entry item ${req.params.id}`);
+	LedgerEntry
+		.findByIdAndUpdate(req.params.id, {$set: req.body}, {new: true})
+		.exec()
+		.then(ledgerEntry => res.status(200).json(ledgerEntry))
+		.catch(err => {
+			res.status(500).json({message: 'Internal server error'});
+		});
+});
+
 
 
 
