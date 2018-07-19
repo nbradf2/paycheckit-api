d2fba747fd5df5291c563a7ec310111c29a89a24
Nicole Bradford <nbradf2@gmail.com>
2018-07-18 19:44:36 -0500

    update client_origin to localhost 3000

--- server.js
+++ server.js
@@ -16,13 +16,23 @@ mongoose.Promise = global.Promise;
 const {PORT, DATABASE_URL} = require('./config');
 const app = express();
 
+app.use(morgan('common'));
+
 app.use(
 	cors({
 		origin: CLIENT_ORIGIN
 	})
 );
 
-app.use(morgan('common'));
+// app.use(function (req, res, next) {
+// 	res.header('Access-Control-Allow-Origin', '*');
+// 	res.header('Access-Control-Allow-Headers', 'Content-Type,Authorization');
+// 	res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE');
+// 	if (req.method === 'OPTIONS') {
+// 		return res.send(204);
+// 	}
+// 	next();
+// });
 
 app.use(passport.initialize());
 passport.use(localStrategy);
